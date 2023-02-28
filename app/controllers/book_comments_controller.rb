cbf60a50a1f4e391d598c0aa63bf027169b17d06
rumi-000 <rumi.sato9797@gmail.com>
2023-02-28 10:29:00 +0000

    Initial Commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,12 +1,19 @@
 class BookCommentsController < ApplicationController
   
   def create
-    book = book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
+    book = Book.find(params[:book_id])
+    comment = BookComment.new(book_comment_params)
+    comment.user_id = current_user.id
     comment.book_id = book.id
     comment.save
-    redirect_to book_path
+    redirect_to book_path(book)
   end
+
+
+  #def destroy
+    #BookComment.find(params[:id]).destroy
+    #redirect_to book_path(params[:book_id])
+  #end
 
   private
 
